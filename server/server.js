aa463a28dc4e5a4a4bdaf71eab4cfd95a9c1656b
Keshav Kumar <keshav12raju@gmail.com>
2020-09-23 18:48:56 +0530

    nodemon config file added

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const server_port = 3000;
 const app = express();
 
 app.get("/", function (req, res) {
-  res.json({"foo": "bar"});
-})
+  res.json({"foo": "bar", "hey": "no "});
+});
+
 app.listen(server_port, () => {
   console.log("Server is running on", server_port);
-})
+});
